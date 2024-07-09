e70a220bfef960253abbc510a3e6241183eb663c
osomasi <osomasi19@gmail.com>
2024-07-09 10:00:26 +0100

    devmasi

--- app/utils/openai.tsx
+++ app/utils/openai.tsx
@@ -1,7 +1,7 @@
-// import OpenAI from 'openai';
-// const {Configuration, openAIApi} = require("openai")
+import OpenAI from 'openai';
+const {Configuration, openAIApi} = require("openai")
 
-// const configuration = new Configuration({apiKey: process.env.OPEN_API_KEY,});
+const configuration = new Configuration({apiKey: process.env.OPEN_API_KEY,});
 
-// const openai = new openAIApi(configuration);
-// export default openai
+const openai = new openAIApi(configuration);
+export default openai
